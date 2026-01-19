5c294661d4c73617d20f1a2ed4517a8275f3cdfb
fciccioend <39481857+fciccioend@users.noreply.github.com>
2026-01-19 20:54:59 +0100

    Update build.gradle.kts

--- StreamingCommunity/build.gradle.kts
+++ StreamingCommunity/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     description = "Film e SerieTV da StreamingCommunity"
-    authors = listOf("doGior","DieGon")
+    authors = listOf("doGior","-END-")
 
     /**
     * Status int as the following:
