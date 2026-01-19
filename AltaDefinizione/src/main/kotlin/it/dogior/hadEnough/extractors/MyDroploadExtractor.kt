5c2fe98f59cf6526152cecbf9690d577e0558f6f
fciccioend <f.bucaria@hotmail.com>
2026-01-19 20:25:46 +0100

    Pulizia

--- AltaDefinizione/src/main/kotlin/it/dogior/hadEnough/extractors/MyDroploadExtractor.kt
+++
@@ -1,7 +0,0 @@
-package it.dogior.hadEnough.extractors
-
-import com.lagradost.cloudstream3.extractors.Supervideo
-
-class MySupervideoExtractor : Supervideo() {
-    override var mainUrl = "supervideo.cc"
-}
